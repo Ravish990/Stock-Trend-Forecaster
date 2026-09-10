23428cd7ec696f7b2115f26812fbad41dcf9d865
Ravish Kumar <ravish.kumar@chromium.org>
2026-09-10 16:52:27 +0530

    added more features to the target

--- Data_Processing/target_data.py
+++ Data_Processing/target_data.py
@@ -9,5 +9,28 @@ df["next_return"] = (
 
 df["target"] = (df["next_return"] > 0).astype(int)
 
+df["return_1d"] = df.groupby("Ticker")["Close"].pct_change()
+
+df["return_5d"] = df.groupby("Ticker")["Close"].pct_change(5)
+
+df["MA5"] = (
+    df.groupby("Ticker")["Close"]
+    .transform(lambda x: x.rolling(5).mean())
+)
+
+df["MA20"] = (
+    df.groupby("Ticker")["Close"]
+    .transform(lambda x: x.rolling(20).mean())
+)
+
+df["volatility_5d"] = (
+    df.groupby("Ticker")["return_1d"]
+    .transform(lambda x: x.rolling(5).std())
+)
+
+df["volume_change"] = (
+    df.groupby("Ticker")["Volume"].pct_change()
+)
+
 print("Target variable created. Here are the first few rows:")
-print(df.head())
+print(df.head(30))
